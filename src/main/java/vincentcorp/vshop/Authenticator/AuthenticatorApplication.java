ef27224f4b9c8dd2c66583fb2051423e6f0ef8fb
Vincent Bui <vincentbuiwork@gmail.com>
2023-02-03 08:40:34 -0500

    Initial commit

---
+++ src/main/java/vincentcorp/vshop/Authenticator/AuthenticatorApplication.java
@@ -0,0 +1,13 @@
+package vincentcorp.vshop.Authenticator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticatorApplication.class, args);
+	}
+
+}

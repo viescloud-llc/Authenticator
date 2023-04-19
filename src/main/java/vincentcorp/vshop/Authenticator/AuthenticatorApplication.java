73cf2c0f794a501d8a4800ab54aead7852158887
VincentB00 <vincentbui1999@gmail.com>
2023-04-19 18:28:22 -0400

    add health check endpoint

--- src/main/java/vincentcorp/vshop/Authenticator/AuthenticatorApplication.java
+++ src/main/java/vincentcorp/vshop/Authenticator/AuthenticatorApplication.java
@@ -1,14 +1,26 @@
 package vincentcorp.vshop.Authenticator;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@RestController
 public class AuthenticatorApplication {
+
+	@Value("${spring.profiles.active}")
+	private String env = "dev";
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthenticatorApplication.class, args);
+	}
+
+	@GetMapping("/_status/healthz")
+	public String healthCheck() {
+		return String.format("Authenticator %s is up and running", env);
 	}
 }

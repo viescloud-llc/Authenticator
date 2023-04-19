e50226eea6d42be7d28b4ff82c63a03ba4205072
VincentB00 <vincentbui1999@gmail.com>
2023-04-19 17:52:10 -0400

    remove test

--- src/test/java/vincentcorp/vshop/Authenticator/JwtTest.java
+++ src/test/java/vincentcorp/vshop/Authenticator/JwtTest.java
@@ -8,17 +8,12 @@ import vincentcorp.vshop.Authenticator.model.User;
 import vincentcorp.vshop.Authenticator.util.JwtTokenUtil;
 
 @SpringBootTest
-@Slf4j
 public class JwtTest 
 {
 
     @Test
     public void JWTGenerator()
     {
-        User user = new User();
-        JwtTokenUtil jwtTokenUtil = new JwtTokenUtil();
-
-        user.setUsername("TEST");
-        user.setPassword("TEST");
+        
     }
 }

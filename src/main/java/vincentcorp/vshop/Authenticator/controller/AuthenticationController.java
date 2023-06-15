02f70a4072fbc926f7fd5c2657eed518edc004f6
VincentB00 <vincentbui1999@gmail.com>
2023-06-15 14:28:33 -0400

    fix patch login user

--- src/main/java/vincentcorp/vshop/Authenticator/controller/AuthenticationController.java
+++ src/main/java/vincentcorp/vshop/Authenticator/controller/AuthenticationController.java
@@ -85,7 +85,7 @@ public class AuthenticationController
         return response;
     }
 
-    @PutMapping("/patch/user")
+    @PatchMapping("/user")
     public User patchUser(@RequestHeader("Authorization") String jwt, @RequestBody User user) {
         User jUser = this.jwtService.getUser(jwt);
         var response = this.userService.patchUser(jUser.getId(), user);
